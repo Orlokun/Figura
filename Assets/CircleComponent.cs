2d125f6df4a7ddaf43b74f715f077e1e202ce571
OrloKun <orlocuario@gmail.com>
2020-09-21 18:46:40 -0300

    figura first step

---
+++ Assets/CircleComponent.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+
+public class CircleComponent : MonoBehaviour
+{
+    int segments;
+    float rangeRadius;
+
+    LineRenderer line;
+
+    void Awake()
+    {
+        line = GetComponent<LineRenderer>();
+        SetTransformDefaultState();
+    }
+
+
+    public void SetCircleData(Circle _cComponent)
+    {
+        segments = _cComponent.segments;
+        rangeRadius = _cComponent.xRadius;
+
+        line.positionCount = segments + 1;
+        line.useWorldSpace = false;
+    }
+
+    public void DrawCircleLine()
+    {
+        float x;
+        float y;
+        float z = 0;
+
+        float angle = 20f;
+
+        for (int i = 0; i < (segments + 1); i++)
+        {
+            x = Mathf.Sin(Mathf.Deg2Rad * angle) * rangeRadius;
+            y = Mathf.Cos(Mathf.Deg2Rad * angle) * rangeRadius;
+
+            line.SetPosition(i, new Vector3(x, y, z));
+
+            angle += (360f / segments);
+        }
+
+        line.startWidth = 0.1f;
+        line.endWidth = 0.1f;
+        line.startColor = new Color(176, 9, 9);
+        line.endColor = new Color(176, 9, 9);
+    }
+
+    private void SetTransformDefaultState()
+    {
+        //transform.position = new Vector3(0,1f,0);
+        transform.Rotate(90f, 0f, 0f);
+    }
+}
---
+++ Assets/VisualizationScripts/VisualBase.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Circle
+{
+    [Range(0, 50)]
+    public int segments;
+
+    [Range(0, 10)]
+    public float xRadius;
+    [Range(0, 10)]
+    public float yRadius;
+
+    public Circle(int _segments, int _xRadius, int _yRadius)
+    {
+        segments = _segments;
+        xRadius = _xRadius;
+        yRadius = _yRadius;
+    }
+}
+
+public class VisualBase : MonoBehaviour
+{
+
+
+    [SerializeField]
+    private int maxRadius = 10;
+    [Range(0, 50)]
+    public int segments = 50;
+    [Range(0, 10)]
+    private float xRadius;
+    [Range(0, 10)]
+    private float yRadius;
+
+    public int numberOfRanges;
+
+    public Circle[] circles;
+
+
+    void Start()
+    {
+        CheckRanges();
+        CreateCircles();
+    }
+
+    void CheckRanges()
+    {
+        bool checkRangeAmount = false;
+        bool hasMaxRadius = false;
+
+        if (numberOfRanges == 0)
+        {
+            Debug.LogError("There is no amount of ranges assigned. Will proceed to Default");
+            checkRangeAmount = false;
+        }
+        else
+        {
+            checkRangeAmount = true;
+        }
+
+        if (maxRadius == 0)
+        {
+            Debug.LogError("There is no max Radius assigned. Will proceed to Default");
+        }
+        else
+        {
+            hasMaxRadius = true;
+        }
+
+        if (checkRangeAmount != true || hasMaxRadius != true)
+        {
+            SetDefaultValues();
+        }
+
+    }
+
+    void CreateCircles()
+    {
+        circles = new Circle[numberOfRanges];
+
+        for (int i = 0; i < circles.Length; i++)
+        {
+            int rangeRadius = (maxRadius / circles.Length) * (i + 1);
+            circles[i] = new Circle(segments, rangeRadius, rangeRadius);
+            CircleComponent cComponent = InstantiateCircle(circles[i]);
+            cComponent.DrawCircleLine();
+        }
+    }
+
+    void SetDefaultValues()
+    {
+        numberOfRanges = 5;
+        maxRadius = 10;
+    }
+
+    CircleComponent InstantiateCircle(Circle _circle)
+    {
+        GameObject circle = (GameObject)Instantiate(Resources.Load("RangeCircle"));                 //Check hardcoded name
+        CircleComponent _cComponent = circle.GetComponent<CircleComponent>();
+        _cComponent.SetCircleData(_circle);
+        return _cComponent;
+    }
+}
