340592e15db001173b7ec6d9627f6a38a09fe30e
Orlando Guerrero <oguerrerofarias@gmail.com>
2020-10-05 20:10:03 -0300

    init scene for mockup

--- Assets/Scripts/UI/UIController.cs
+++ Assets/Scripts/UI/UIController.cs
@@ -5,13 +5,11 @@ using UnityEngine;
 public class UIController : MonoBehaviour
 {
 
-    private DimensionsArchitect dArchitect;
     private DataUnitsManager dUManager;
 
     // Start is called before the first frame update
     void Awake()
     {
-        dArchitect = FindObjectOfType<DimensionsArchitect>();
         dUManager = FindObjectOfType<DataUnitsManager>();
     }
 
