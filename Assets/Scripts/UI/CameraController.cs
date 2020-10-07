09a3cf3b53c90adcb77f2afc991b5de1678dcc63
Orlando Guerrero <oguerrerofarias@gmail.com>
2020-10-07 20:27:11 -0300

    its getting better. Now the model rotates and i can view different types of charts. Must configure instantiate new graph

---
+++ Assets/Scripts/UI/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    LayerMask radarGraphs;
+    LayerMask tdGraphs;
+
+    Camera camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = Camera.main;
+        Debug.Log("TheCamera Culling mask is: " + camera.cullingMask);
+        camera.cullingMask = radarGraphs.value;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/UI/TestMenus/TestMenu.cs
+++ Assets/Scripts/UI/TestMenus/TestMenu.cs
@@ -21,9 +21,8 @@ namespace UnityCore
 
                 }
             }
-
-        }
 #endif
+        }
     }
 }
 
