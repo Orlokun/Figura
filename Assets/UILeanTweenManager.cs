1c049ef7fbc43edb21f55914835aca6d2c0a491d
Orlando Guerrero <oguerrerofarias@gmail.com>
2020-11-19 20:05:21 -0300

    UI resized

---
+++ Assets/UILeanTweenManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UILeanTweenManager : MonoBehaviour
+{
+    public static UILeanTweenManager instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    public static void 
+}
